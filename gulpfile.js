63807dbaa49e1bab018f31f7db048f26771cd63e
Mark F. Ellis II <ellismarkf@gmail.com>
2016-04-28 23:44:55 -0500

    add less processing and live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
-var gulp = require("gulp")
+var gulp = require('gulp')
+var less = require('gulp-less')
+var path = require('path')
+var browserSync = require('browser-sync')
+var reload = browserSync.reload
 
-gulp.task('default', function() {
-	
+gulp.task('less', () => {
+	return gulp.src('./style/**/*.less')
+		.pipe(less({
+			paths: [ path.join(__dirname, 'style', 'includes') ]
+		}))
+		.pipe(gulp.dest('./style.css'))
+		.pipe(reload({ stream: true }))
 })
+
+gulp.task('serve', ['less'], () => {
+	browserSync({
+		server: {
+			baseDir: __dirname
+		}
+	})
+	gulp.watch('./style/**/*.less', ['less'])
+	gulp.watch('*.html').on('change', reload)
+})
+
+
+gulp.task('default', ['less', 'serve'])
