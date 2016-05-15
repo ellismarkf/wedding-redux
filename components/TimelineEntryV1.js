61cf8f912aefdd437b51484f0ec0a3d52e0cb166
Mark F. Ellis II <ellismarkf@gmail.com>
2016-05-15 00:15:25 -0500

    remove unnecessary files

--- components/TimelineEntryV1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TimelineEntryV1 = () => (
-	<div id="m1" className="timeline-entry-v1">
-		<h2>Houston, TX</h2>
-		<h3>Summer, 2012</h3>
-		<p>he was just starting to get fascinated with web development, and fed up with his make-shift career in marketing</p>
-	</div>
-)
--- reducers/index.js
+++ reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 import locale from './locale'
-import carousel from './carousel'
 import rsvp from './rsvp'
 
 const weddingApp = combineReducers({
 	locale,
-	carousel,
 	rsvp
 })
 
