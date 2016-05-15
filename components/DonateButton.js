520e701b592e8e4f72bba67acf8bd515a64a1abb
Mark F. Ellis II <ellismarkf@gmail.com>
2016-05-15 15:18:45 -0500

    pull button image from https

--- components/DonateButton.js
+++ components/DonateButton.js
@@ -10,7 +10,7 @@ class DonateButton extends Component {
 					<form className='donateBtn' action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 						<input type="hidden" name="cmd" value="_s-xclick" />
 						<input type="hidden" name="hosted_button_id" value="7FPSX4TPCMDNA" />
-						<input type="image" src="http://wowmarkandray.com/img/contribute-english.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+						<input type="image" src="https://wowmarkandray.com/img/contribute-english.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
 						<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
 					</form>
 				}
@@ -18,7 +18,7 @@ class DonateButton extends Component {
 					<form className='donateBtn' action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 						<input type="hidden" name="cmd" value="_s-xclick" />
 						<input type="hidden" name="hosted_button_id" value="JEYSZYBG4J4WQ" />
-						<input type="image" src="http://wowmarkandray.com/img/contribute-spanish.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+						<input type="image" src="https://wowmarkandray.com/img/contribute-spanish.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
 						<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
 					</form>
 
