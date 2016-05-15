70928a940703d72f5c5e66232f5593807b488934
Mark F. Ellis II <ellismarkf@gmail.com>
2016-05-15 13:02:37 -0500

    update donate buttons, remove forced https

--- components/DonateButton.js
+++ components/DonateButton.js
@@ -7,19 +7,22 @@ class DonateButton extends Component {
 		return (
 			<div>
 				{locale === 'en' &&
-					<form className='donateBtn' action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 						<input type="hidden" name="cmd" value="_s-xclick" />
 						<input type="hidden" name="hosted_button_id" value="7FPSX4TPCMDNA" />
 						<input type="image" src="http://wowmarkandray.com/img/contribute-english.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
 						<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-					</form>}
+					</form>
+				}
 				{locale === 'es' &&
 					<form className='donateBtn' action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 						<input type="hidden" name="cmd" value="_s-xclick" />
-						<input type="hidden" name="hosted_button_id" value="BYJA96R7X4N8A" />
+						<input type="hidden" name="hosted_button_id" value="JEYSZYBG4J4WQ" />
 						<input type="image" src="http://wowmarkandray.com/img/contribute-spanish.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
 						<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-					</form>}
+					</form>
+
+				}
 			</div>
 		)
 	}
